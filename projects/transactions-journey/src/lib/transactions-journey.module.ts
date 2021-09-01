0c1a126a3b9f7e3d2582c9b957c5b4df790899f0
Serhii Koshman <koshman.animys@gmail.com>
2021-09-01 10:38:02 +0200

    upd transactions journey regarding the changes, upd public api with exposing the external communication service

--- projects/transactions-journey/src/lib/transactions-journey.module.ts
+++ projects/transactions-journey/src/lib/transactions-journey.module.ts
@@ -8,7 +8,6 @@ import { AmountModule, InputTextModule } from '@backbase/ui-ang';
 import { TransactionItemComponent } from './components/transaction-item/transaction-item.component';
 import { TextFilterComponent } from './components/text-filter/text-filter.component';
 import { FilterTransactionsPipe } from './pipes/filter-transactions.pipe';
-import { TransactionsJourneyConfiguration } from './transactions-journey-config.service';
 
 const defaultRoute: Route = {
   path: '',
@@ -20,18 +19,11 @@ const defaultRoute: Route = {
 
 @NgModule({
   declarations: [TransactionsViewComponent, TransactionItemComponent, TextFilterComponent, FilterTransactionsPipe],
-  providers: [TransactionsJourneyConfiguration],
-  imports: [
-    CommonModule,
-    RouterModule,
-    FormsModule,
-    AmountModule,
-    InputTextModule,
-  ],
+  imports: [CommonModule, RouterModule, FormsModule, AmountModule, InputTextModule],
 })
 export class TransactionsJourneyModule {
-
-  static forRoot(data: { [key: string]: unknown; route: Route } = { route: defaultRoute }
+  static forRoot(
+    data: { [key: string]: unknown; route: Route } = { route: defaultRoute },
   ): ModuleWithProviders<TransactionsJourneyModule> {
     return {
       ngModule: TransactionsJourneyModule,
--- projects/transactions-journey/src/public-api.ts
+++ projects/transactions-journey/src/public-api.ts
@@ -2,7 +2,9 @@
  * Public API Surface of transactions-journey
  */
 
-export * from './lib/transactions-journey-config.service';
+export * from './lib/services/transactions-journey-config.service';
 export * from './lib/transactions-journey.module';
 export * from './lib/views/transactions-view/transactions-view.component';
 export type { Transaction } from './lib/model/transaction';
+
+export { TransactionsCommunicationService, TRANSACTIONS_JOURNEY_COMMUNICATION_SERIVCE } from './lib/communication';
