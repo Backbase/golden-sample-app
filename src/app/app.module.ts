e256e14c63ec2c6f983b1e9a1f50f4b8f27bb3ce
jorge <jorge@backbase.com>
2021-09-06 14:18:53 +0200

    remove hack to use entitlements service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { EntitlementsModule } from '@backbase/foundation-ang/entitlements';
 import { OAuthModule } from 'angular-oauth2-oidc';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthGuard } from './auth.guard';
-import { WebSdkModule } from '@backbase/foundation-ang/web-sdk';
+import { WebSdkApiModule, WebSdkModule } from '@backbase/foundation-ang/web-sdk';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -38,15 +38,7 @@ import { JourneyCommunicationService } from './services/journey-communication.se
     LogoModule,
     NgbDropdownModule,
     AvatarModule,
-    WebSdkModule.forRoot({
-      cx: {
-        // TODO: this is a hack to get entitlements work using websdk
-        entitlementsEnable: true,
-        entitlementsUri: '/entitlements',
-        clientId: 'client',
-        realm: 'realm',
-      },
-    } as any),
+    WebSdkApiModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
   ],
