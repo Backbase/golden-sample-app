962ebf1974282d59ad03eac7ab1fd9c79ab583a7
Serhii Koshman <koshman.animys@gmail.com>
2021-09-09 14:35:50 +0300

    upd password input to be actual password input

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ButtonModule, InputTextModule, InputValidationMessageModule } from '@backbase/ui-ang';
+import { ButtonModule, InputPasswordModule, InputTextModule, InputValidationMessageModule } from '@backbase/ui-ang';
 
 @NgModule({
   declarations: [LoginPageComponent],
@@ -14,6 +14,7 @@ import { ButtonModule, InputTextModule, InputValidationMessageModule } from '@ba
     InputValidationMessageModule,
     ButtonModule,
     InputTextModule,
+    InputPasswordModule,
   ],
 })
 export class LoginModule {}
