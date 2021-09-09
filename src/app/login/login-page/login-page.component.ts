5cb29dedd73cc8bed867844a7b280c7011c1e921
Serhii Koshman <koshman.animys@gmail.com>
2021-09-09 14:15:19 +0300

    upd login form to use Inputs and button from designsystem, add validation message for login field

--- src/app/login/login-page/login-page.component.ts
+++ src/app/login/login-page/login-page.component.ts
@@ -1,19 +1,25 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AppAuthService } from '../../services/app-auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
 })
-export class LoginPageComponent {
-  login = '';
+export class LoginPageComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor(private authService: AppAuthService) {}
+  constructor(private authService: AppAuthService, private formBuilder: FormBuilder) {}
 
-  onSubmit(loginForm: NgForm) {
-    if (loginForm.valid) {
-      const { login }: { login: string } = loginForm.value;
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      login: ['', [Validators.required, Validators.minLength(4)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { login }: { login: string } = this.loginForm.value;
       this.authService.login({
         redirectUri: 'transactions',
         login: login.trim().toLowerCase(),
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginRoutingModule } from './login-routing.module';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule, InputTextModule, InputValidationMessageModule } from '@backbase/ui-ang';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule, LoginRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    InputValidationMessageModule,
+    ButtonModule,
+    InputTextModule,
+  ],
 })
 export class LoginModule {}
