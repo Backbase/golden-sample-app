2f392616349cc3174d5aa67051268d239da05530
Serhii Koshman <koshman.animys@gmail.com>
2021-09-09 14:53:55 +0300

    add header to login page, upd password input to be password instead of input type=text

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,7 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ButtonModule, InputPasswordModule, InputTextModule, InputValidationMessageModule } from '@backbase/ui-ang';
+import {
+  ButtonModule,
+  HeaderModule,
+  InputPasswordModule,
+  InputTextModule,
+  InputValidationMessageModule,
+} from '@backbase/ui-ang';
 
 @NgModule({
   declarations: [LoginPageComponent],
@@ -15,6 +21,7 @@ import { ButtonModule, InputPasswordModule, InputTextModule, InputValidationMess
     ButtonModule,
     InputTextModule,
     InputPasswordModule,
+    HeaderModule,
   ],
 })
 export class LoginModule {}
