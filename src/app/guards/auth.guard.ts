fc879e3a5b93f7850b10275eb8046af8887458c6
Serhii Koshman <koshman.animys@gmail.com>
2021-09-09 09:51:23 +0300

    move auth guard to a separate folder, upd it to be in sync with auth service

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AppAuthService } from '../services/app-auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private authService: AppAuthService) {}
+
+  canActivate(_next: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.isAuthenticated$.pipe(
+      tap((isAuthenticated) => {
+        if (!isAuthenticated) {
+          this.router.navigate(['login']);
+        }
+      }),
+    );
+  }
+}
