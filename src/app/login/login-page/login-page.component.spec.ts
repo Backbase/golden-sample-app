ac5f7cd2dc4db3c8975ec43de0799e37d282982a
Serhii Koshman <koshman.animys@gmail.com>
2021-09-15 13:57:28 +0300

    upd login page unit tests

--- src/app/login/login-page/login-page.component.spec.ts
+++ src/app/login/login-page/login-page.component.spec.ts
@@ -1,23 +1,18 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { AppAuthService } from 'src/app/services/app-auth.service';
 
 import { LoginPageComponent } from './login-page.component';
 
-import { InputTextModule, InputValidationMessageModule } from '@backbase/ui-ang';
-
-fdescribe('LoginPageComponent', () => {
+describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
 
   let authServiceStub: jasmine.SpyObj<AppAuthService>;
 
   const components = {
-    loginInput: () => fixture.debugElement.query(By.css('[data-role="login-input"]')),
-    loginForm: () => fixture.debugElement.query(By.css('[data-role="login-form"]')),
-    loginValidationMessage: () => fixture.debugElement.query(By.css('[data-role="login-validation-message"]')),
+    loginBtn: () => fixture.debugElement.query(By.css('[data-role="login-button"]')),
   };
 
   beforeEach(() => {
@@ -25,7 +20,6 @@ fdescribe('LoginPageComponent', () => {
 
     TestBed.configureTestingModule({
       declarations: [LoginPageComponent],
-      imports: [ReactiveFormsModule, InputTextModule, InputValidationMessageModule],
       providers: [
         {
           provide: AppAuthService,
@@ -40,39 +34,11 @@ fdescribe('LoginPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should pass login to authentication service', () => {
-    const mockLoginValue = 'mock login';
+  it('should call authentication service after btn click', () => {
+    components.loginBtn().triggerEventHandler('click', {});
 
-    submitLoginFromWith(mockLoginValue);
-
-    expect(authServiceStub.login).toHaveBeenCalledWith({
-      redirectUri: 'transactions',
-      login: mockLoginValue,
-    });
-  });
-
-  describe('validation message should show', () => {
-    it('if the login is less then 4 characters', () => {
-      const shortLogin = 'log';
-
-      submitLoginFromWith(shortLogin);
-
-      expect(components.loginValidationMessage()).toBeTruthy();
-      expect(authServiceStub.login).not.toHaveBeenCalled();
-    });
-
-    it('if user is trying to submit the empty login', () => {
-      submitLoginFromWith('');
-
-      expect(components.loginValidationMessage()).toBeTruthy();
-      expect(authServiceStub.login).not.toHaveBeenCalled();
-    });
-  });
-
-  function submitLoginFromWith(login: string) {
-    component.loginForm.controls['login'].setValue(login);
     fixture.detectChanges();
 
-    components.loginForm().triggerEventHandler('ngSubmit', {});
-  }
+    expect(authServiceStub.login).toHaveBeenCalled();
+  });
 });
