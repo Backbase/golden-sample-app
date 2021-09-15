c28ae4392a3d80c2e9d18e7dd22803abf8605076
Serhii Koshman <koshman.animys@gmail.com>
2021-09-15 10:23:21 +0300

    upd app-auth service to use oauth oidc complient service under the hood

--- src/app/services/app-auth.service.ts
+++ src/app/services/app-auth.service.ts
@@ -1,71 +1,158 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { OAuthErrorEvent, OAuthService } from 'angular-oauth2-oidc';
 import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-export type User = {
-  id: string;
-  name: string;
-  balance: number;
-};
 
 /**
- * This is not the actual implementation of authentication service,
- * this is only example, be careful with reading all of the notes inside.
+ *
+ * This service is build with external OIDC compliant OAuthService
+ * for demonstration purposes, be careful and read all of the comments
+ *
  */
 @Injectable({
   providedIn: 'root',
 })
 export class AppAuthService {
   private isAuthenticated$$ = new BehaviorSubject<boolean>(false);
-
-  private currentUser$$ = new BehaviorSubject<User | null>(null);
-  public currentUser$ = this.currentUser$$.asObservable();
-
-  /**
-   * In the production version of auth service it should be like
-   * public isAuthenticated$ = this.authServiceFromFoundation.isAuthenticated;
-   */
   public isAuthenticated$ = this.isAuthenticated$$.asObservable();
 
-  /**
-   * In the production version of app auth service there will be only one dependency
-   * for authService from foundation-ang
-   */
-  constructor(private http: HttpClient, private router: Router) {}
+  private isDoneLoading$$ = new BehaviorSubject<boolean>(false);
+  public isDoneLoading$ = this.isDoneLoading$$.asObservable();
 
-  /**
-   * In the production version only `redirectUri` is needed to be provided as an argument for
-   * this.authServiceFromFoundation.login({ redirectUri }) method call,
-   * and the authentication request will be handled by identity provided from the settings
-   */
-  login({ login, redirectUri }: { login: string; redirectUri: string }): void {
-    this.http
-      .post('/api/auth/login', { login, redirectUri })
-      .pipe(
-        map((user) => this.currentUser$$.next(user as User)),
-        map(() => this.isAuthenticated$$.next(true)),
-      )
-      .subscribe(() => this.redirectTo(redirectUri));
+  constructor(private router: Router, private oauthService: OAuthService) {
+    /**
+     * This is needed only for developer purposes
+     * to explore all of the Oauth events
+     */
+    this.oauthService.events.subscribe((event) => {
+      if (event instanceof OAuthErrorEvent) {
+        console.error('OAuthErrorEvent Object:', event);
+      } else {
+        console.warn('OAuthEvent Object:', event);
+      }
+    });
+
+    /**
+     * After successful auh and the `token_received` event
+     * it will updated isAuthenticated$ to activate the routes
+     */
+    this.oauthService.events.subscribe((_) => {
+      this.isAuthenticated$$.next(this.oauthService.hasValidAccessToken());
+    });
+
+    /**
+     * Without this setup the silent refresh request
+     * will always end up unsunsuccessfully
+     */
+    this.oauthService.setupAutomaticSilentRefresh();
   }
 
   /**
-   * In the production version only `redirectUri` is needed to be provided as an argument for
-   * this.authServiceFromFoundation.logout({ redirectUri }) method call
+   * This is method that will set up oathService to be ready
+   * to work with identity provider, it is needed
+   * because of the `angular-oauth2-oidc` library inner structure
    */
-  logout(logoutOptions?: { redirectUri: string }): void {
-    const redirectUri = logoutOptions?.redirectUri || 'login';
-    this.http
-      .post('/api/auth/logout', { redirectUri })
-      .pipe(
-        map(() => this.currentUser$$.next(null)),
-        map(() => this.isAuthenticated$$.next(false)),
-      )
-      .subscribe(() => this.redirectTo(redirectUri));
+  public runInitialLoginSequence(): Promise<void> {
+    // 0. LOAD CONFIG:
+    // First we have to check to see how the IdServer is
+    // currently configured:
+    return (
+      this.oauthService
+        .loadDiscoveryDocument()
+
+        // For demo purposes, we pretend the previous call was very slow
+        .then(() => new Promise<void>((resolve) => setTimeout(() => resolve(), 1000)))
+
+        // 1. HASH LOGIN:
+        // Try to log in via hash fragment after redirect back
+        // from IdServer from initImplicitFlow:
+        .then(() => this.oauthService.tryLogin())
+
+        .then(() => {
+          if (this.oauthService.hasValidAccessToken()) {
+            return Promise.resolve();
+          }
+
+          // 2. SILENT LOGIN:
+          // Try to log in via a refresh because then we can prevent
+          // needing to redirect the user:
+          // return Promise.resolve();
+          return (
+            this.oauthService
+              .silentRefresh()
+              // .then(() => new Promise<void>((resolve) => setTimeout(() => resolve(), 1000)))
+
+              .then(() => Promise.resolve())
+              .catch((result) => {
+                // debugger;
+                // Subset of situations from https://openid.net/specs/openid-connect-core-1_0.html#AuthError
+                // Only the ones where it's reasonably sure that sending the
+                // user to the IdServer will help.
+                const errorResponsesRequiringUserInteraction = [
+                  'interaction_required',
+                  'login_required',
+                  'account_selection_required',
+                  'consent_required',
+                ];
+
+                if (
+                  result &&
+                  result.reason &&
+                  errorResponsesRequiringUserInteraction.indexOf(result.reason.error) >= 0
+                ) {
+                  // 3. ASK FOR LOGIN:
+                  // At this point we know for sure that we have to ask the
+                  // user to log in, so we redirect them to the IdServer to
+                  // enter credentials.
+                  //
+                  // Enable this to ALWAYS force a user to login.
+                  // this.login();
+                  //
+                  // Instead, we'll now do this:
+                  console.warn('User interaction is needed to log in, we will wait for the user to manually log in.');
+                  return Promise.resolve();
+                }
+
+                // We can't handle the truth, just pass on the problem to the
+                // next handler.
+                return Promise.reject(result);
+              })
+          );
+        })
+        .then(() => {
+          this.isDoneLoading$$.next(true);
+
+          // Check for the strings 'undefined' and 'null' just to be sure. Our current
+          // login(...) should never have this, but in case someone ever calls
+          // initImplicitFlow(undefined | null) this could happen.
+          if (
+            this.oauthService.state &&
+            this.oauthService.state !== 'undefined' &&
+            this.oauthService.state !== 'null'
+          ) {
+            let stateUrl = this.oauthService.state;
+            if (stateUrl.startsWith('/') === false) {
+              stateUrl = decodeURIComponent(stateUrl);
+            }
+            console.log(`There was state of ${this.oauthService.state}, so we are sending you to: ${stateUrl}`);
+            this.router.navigate(['login']);
+          }
+        })
+        .catch(() => this.isDoneLoading$$.next(true))
+    );
   }
 
-  private redirectTo(redirectUri: string): void {
-    this.router.navigate([redirectUri]);
+  /**
+   * This method is only needed to incapsulate the auth logic
+   */
+  public login(): void {
+    this.oauthService.initLoginFlow();
+  }
+
+  /**
+   * This method is only needed to incapsulate the auth logic
+   */
+  public logout(): void {
+    this.oauthService.logOut();
   }
 }
