ced80fa09c6397a3f2d67292e039dc5b847acb7a
Serhii Koshman <koshman.animys@gmail.com>
2021-09-15 10:24:46 +0300

    upd auth guard to work with updated auth service

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { filter, switchMap, tap } from 'rxjs/operators';
 import { AppAuthService } from '../services/app-auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router, private authService: AppAuthService) {}
+  constructor(private authService: AppAuthService) {}
 
   canActivate(): Observable<boolean> {
-    return this.authService.isAuthenticated$.pipe(
-      tap((isAuthenticated) => {
-        if (!isAuthenticated) {
-          this.router.navigate(['login']);
-        }
-      }),
+    return this.authService.isDoneLoading$.pipe(
+      filter((isDone) => isDone),
+      switchMap((_) => this.authService.isAuthenticated$),
+      tap((isAuthenticated) => isAuthenticated || this.authService.login()),
     );
   }
 }
