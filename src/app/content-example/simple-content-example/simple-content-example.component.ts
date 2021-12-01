df6a5ed5e571c5c4ac777e53cb95d95d94ed14e4
Serhii Koshman <koshman.animys@gmail.com>
2021-12-01 19:28:52 +0200

    remove styles file for simple content example component

--- src/app/content-example/simple-content-example/simple-content-example.component.ts
+++ src/app/content-example/simple-content-example/simple-content-example.component.ts
@@ -4,7 +4,6 @@ import { ContentExampleService } from '../content-example.service';
 @Component({
   selector: 'app-simple-content-example',
   templateUrl: './simple-content-example.component.html',
-  styleUrls: ['./simple-content-example.component.sass']
 })
 export class SimpleContentExampleComponent {
   contentFromTheServer$ = this.contentService.simpleContentExample$;
