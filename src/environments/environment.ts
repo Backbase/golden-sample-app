c527859c66fce0454461b3328a405b842dccce6a
Serhii Koshman <koshman.animys@gmail.com>
2021-12-15 16:24:59 +0200

    upd the issuer for identity provider to use the identity sandox

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -30,14 +30,14 @@ export const environment = {
 
 export const authConfig: AuthConfig = {
   // Url of the Identity Provider
-  issuer: 'https://identity-latest-universal.retail.backbase.eu/auth/realms/backbase',
+  issuer: 'https://identity.dev.sdbx.live.backbaseservices.com/auth/realms/retail',
 
   // URL of the SPA to redirect the user to after login
   redirectUri: document.location.origin + '/transactions',
 
   // The SPA's id. The SPA is registered with this id at the auth-server
   clientId: 'bb-web-client',
-  
+
   // Just needed if your auth server demands a secret. In general, this
   // is a sign that the auth server is not configured with SPAs in mind
   // and it might not enforce further best practices vital for security
