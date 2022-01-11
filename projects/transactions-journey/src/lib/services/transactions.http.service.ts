e03c35c9c5715bc8cb844fef83d49019510ef9cc
Serhii Koshman <koshman.animys@gmail.com>
2022-01-11 11:30:42 +0200

    draft of transactions service with 2 requests example

--- projects/transactions-journey/src/lib/services/transactions.http.service.ts
+++ projects/transactions-journey/src/lib/services/transactions.http.service.ts
@@ -1,16 +1,67 @@
 import { Injectable } from '@angular/core';
-import { Transaction } from '../model/transaction';
-import { of } from 'rxjs';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { combineLatest, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { TransactionsJourneyConfiguration } from './transactions-journey-config.service';
 import { map, switchMap } from 'rxjs/operators';
 
+import { GetTransactionsRequestParams, GetTransactionsWithPostRequestParams, TransactionClientHttpService} from '@backbase/data-ang/transactions';
+import { ProductSummaryHttpService } from '@backbase/data-ang/arrangements';
+
 @Injectable()
 export class TransactionsHttpService {
-  public transactions$ = of(this.configurationService.pageSize).pipe(
-    map((pageSize) => new HttpParams().append('page', String(0)).append('pageSize', String(pageSize))),
-    switchMap((params) => this.http.get<Transaction[]>('/api/transactions', { params })),
+  private arrangements$ = this.productSummaryDataHttpService
+    .getArrangementsByBusinessFunction({
+      businessFunction: 'Product Summary',
+      resourceName: 'Product Summary',
+      privilege: 'view',
+      size: 1000000,
+      },
+      'body',
+    )
+    .pipe(
+      map((arrangements) => arrangements.map((ar) => ar.id)),
+    );
+
+  public transactions$ = combineLatest([
+    this.arrangements$,
+    of(this.configurationService.pageSize),
+  ]).pipe(
+    switchMap(
+      ([arrangements, pageSize]) =>
+        this.transactionsHttpService.getTransactions(
+          {
+            transactionListRequest: {
+              arrangementsIds: arrangements,
+              size: pageSize,
+            },
+          } as GetTransactionsRequestParams,
+          'body',
+        ),
+    ),
   );
 
-  constructor(private http: HttpClient, private readonly configurationService: TransactionsJourneyConfiguration) {}
+  // public transactions$ = combineLatest([
+  //   this.arrangements$,
+  //   of(this.configurationService.pageSize),
+  // ]).pipe(
+  //   switchMap(
+  //     ([arrangements, pageSize]) =>
+  //       this.transactionsHttpService.getTransactionsWithPost(
+  //         {
+  //           transactionListRequest: {
+  //             arrangementsIds: arrangements,
+  //             size: pageSize,
+  //           },
+  //         } as GetTransactionsWithPostRequestParams,
+  //         'response',
+  //       ),
+  //   ),
+  // );
+
+  constructor(
+    private http: HttpClient,
+    private readonly configurationService: TransactionsJourneyConfiguration,
+    private transactionsHttpService: TransactionClientHttpService,
+    private readonly productSummaryDataHttpService: ProductSummaryHttpService,
+  ) {}
 }
