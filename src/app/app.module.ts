95b0a2f97091e86e527939ea3d97d1251f67c29b
Serhii Koshman <koshman.animys@gmail.com>
2022-01-11 10:40:20 +0200

    change order of imports to have clear separation, make mockProviders optional, provide base path for transactions and arrangemenet data modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_INITIALIZER, NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { authConfig, environment } from '../environments/environment';
-import { EntitlementsModule } from '@backbase/foundation-ang/entitlements';
 import { HttpClientModule } from '@angular/common/http';
-import { AuthGuard } from './guards/auth.guard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { EntitlementsModule } from '@backbase/foundation-ang/entitlements';
+
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+
 import { AuthConfig, OAuthModule, OAuthModuleConfig, OAuthService, OAuthStorage } from 'angular-oauth2-oidc';
+
 import { DropdownMenuModule } from '@backbase/ui-ang/dropdown-menu';
 import { IconModule } from '@backbase/ui-ang/icon';
 import { LayoutModule } from '@backbase/ui-ang/layout';
 import { LogoModule } from '@backbase/ui-ang/logo';
 import { AvatarModule } from '@backbase/ui-ang/avatar';
+
 import { WebSdkModule } from '@backbase/foundation-ang/web-sdk';
+
+import { TRANSACTIONS_BASE_PATH } from '@backbase/data-ang/transactions';
+import { ARRANGEMENT_MANAGER_BASE_PATH } from '@backbase/data-ang/arrangements';
+
+import { authConfig, environment } from '../environments/environment';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthGuard } from './guards/auth.guard';
 import { LocaleSelectorModule } from './locale-selector/locale-selector.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [ AppComponent ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -42,10 +52,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     }),
     WebSdkModule.forRoot({
       apiRoot: environment.apiRoot,
-    })
+    }),
   ],
   providers: [
-    ...environment.mockProviders,
+    ...(environment.mockProviders || []),
     AuthGuard,
     { provide: AuthConfig, useValue: authConfig },
     {
@@ -63,12 +73,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       multi: true,
       deps: [ OAuthService ],
       useFactory: (oAuthService: OAuthService) => () =>
-        oAuthService.loadDiscoveryDocumentAndTryLogin().then(() =>
-          oAuthService.setupAutomaticSilentRefresh()
-        )
-    }
+        oAuthService.loadDiscoveryDocumentAndTryLogin().then(() => oAuthService.setupAutomaticSilentRefresh()),
+    },
+    {
+      provide: TRANSACTIONS_BASE_PATH,
+      useValue: environment.apiRoot + '/transaction-manager',
+    },
+    {
+      provide: ARRANGEMENT_MANAGER_BASE_PATH,
+      useValue: environment.apiRoot + '/arrangement-manager',
+    },
   ],
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
