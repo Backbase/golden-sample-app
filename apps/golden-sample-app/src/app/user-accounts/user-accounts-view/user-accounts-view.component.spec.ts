89c7c96b593dfe9be8e90e481eea9f1b179a95b2
Serhii Koshman <koshman.animys@gmail.com>
2022-02-10 10:01:12 +0200

    remove styles file for user-accounts-view component because it is not needed, add UT for rendering

--- apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.spec.ts
+++ apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.spec.ts
@@ -1,25 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { UserAccountsService } from '../user-accounts.service';
 import { UserAccountsViewComponent } from './user-accounts-view.component';
 
 describe('UserAccountsViewComponent', () => {
   let component: UserAccountsViewComponent;
   let fixture: ComponentFixture<UserAccountsViewComponent>;
 
+  const mockUserAccountsService = {
+    arrangements$: of([{
+      displayName: 'mock display name',
+      availableBalance: 0,
+      currency: 'EUR' 
+    }, {
+      displayName: 'mock display name',
+      availableBalance: 0,
+      currency: 'EUR' 
+    }]),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserAccountsViewComponent ]
+      declarations: [ UserAccountsViewComponent ],
+      providers: [
+        { provide: UserAccountsService, useValue: mockUserAccountsService }
+      ]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(UserAccountsViewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should render correct ammount of available accounts', () => {
+    const accounts = fixture.nativeElement.querySelectorAll('[data-role="arrangmenet-container"]');
+
+    expect(accounts.length).toBe(2);
+  })
 });
--- apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.ts
+++ apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.ts
@@ -4,10 +4,8 @@ import { UserAccountsService } from '../user-accounts.service';
 @Component({
   selector: 'app-user-accounts-view',
   templateUrl: './user-accounts-view.component.html',
-  styleUrls: ['./user-accounts-view.component.sass']
 })
 export class UserAccountsViewComponent {
-
   public arrangements$ = this.userAccountsService.arrangements$
 
   constructor(private readonly userAccountsService: UserAccountsService) { }
