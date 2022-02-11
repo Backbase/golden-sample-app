57aaba96a018e468ff1bdd241392bd894cf9e376
Serhii Koshman <koshman.animys@gmail.com>
2022-02-11 11:53:33 +0200

    fix journey communication service UT

--- apps/golden-sample-app/src/app/services/journey-communication.service.spec.ts
+++ apps/golden-sample-app/src/app/services/journey-communication.service.spec.ts
@@ -19,8 +19,9 @@ describe('JourneyCommunicationService', () => {
 
   it('should add transfer to latests transactions', (done) => {
     service.latestTransaction$.subscribe((received) => {
-      expect(received?.transaction.amountCurrency.amount).toEqual(mockAmount);
-      expect(received?.merchant.name).toEqual(mocktoAccount);
+      console.log(received);
+      expect(received?.transactionAmountCurrency.amount).toEqual(String(mockAmount));
+      expect(received?.merchant?.name).toEqual(mocktoAccount);
       done();
     });
   });
