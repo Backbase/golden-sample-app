fd89291dfca0b26bc5661c742973e22f5cd6b5a6
Serhii Koshman <koshman.animys@gmail.com>
2022-02-14 23:27:35 +0200

    remove comment about future release of the user-context widget, because it is not informative

--- apps/golden-sample-app/src/app/user-context/user-context.component.ts
+++ apps/golden-sample-app/src/app/user-context/user-context.component.ts
@@ -2,10 +2,6 @@ import { Component } from '@angular/core';
 import { LOGOUT } from '@backbase/foundation-ang/web-sdk';
 import { OAuthService } from 'angular-oauth2-oidc';
 
-// This is temporary solution,
-// that will be removed after `@backbase/select-context-widget-ang`
-// will be updated in the next release
-
 const logoutFactoryService = (oAuthService: OAuthService) => {
   return {
     goToLoginPage: () => oAuthService.initLoginFlow(),
