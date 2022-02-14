f5711c2de6e1925c0e37553bde93e1e55590f490
Serhii Koshman <koshman.animys@gmail.com>
2022-02-14 23:18:59 +0200

    remove unused entitlement

--- apps/golden-sample-app/src/app/services/entitlementsTriplets.ts
+++ apps/golden-sample-app/src/app/services/entitlementsTriplets.ts
@@ -1,7 +1,5 @@
 export const triplets = {
   canViewTransactions: 'Transactions.Transactions.view',
-  // todo: what is the the correct function?
-  // canViewTransfer: 'Payments.transfer.view',
   canMakeLimitlessAmountTransfer: 'Payments.transfer.limitless',
   canViewPositivePay: 'Payments.ManagePositivePay.view',
   canEditPositivePay: 'Payments.ManagePositivePay.create',
