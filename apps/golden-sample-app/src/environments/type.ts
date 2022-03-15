c046ef75875e19ddf2652e9c8334b0d84fe8520f
jorge <jorge@backbase.com>
2022-03-15 14:55:47 +0100

    add typing to common attribute in environment variables

--- apps/golden-sample-app/src/environments/type.ts
+++ apps/golden-sample-app/src/environments/type.ts
@@ -1,9 +1,13 @@
 import { Provider } from '@angular/core';
 
+export interface sharedJourneyConfiguration {
+  designSlimMode: boolean;
+}
+
 export interface Environment {
   production: boolean;
   apiRoot: string;
   locales: string[];
   mockProviders?: Provider[];
-  common: any;
+  common: sharedJourneyConfiguration;
 }
