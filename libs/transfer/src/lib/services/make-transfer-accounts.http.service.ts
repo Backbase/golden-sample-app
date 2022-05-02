4ad0ea1ebb336bc85d64154aa20f0d5dc722aea9
Jorge Maldonado <georgejolema@gmail.com>
2022-05-02 09:20:20 +0200

    Update libs/transfer/src/lib/services/make-transfer-accounts.http.service.ts
    Co-authored-by: Kirill Sudarushkin <kirills@backbase.com>

--- libs/transfer/src/lib/services/make-transfer-accounts.http.service.ts
+++ libs/transfer/src/lib/services/make-transfer-accounts.http.service.ts
@@ -26,10 +26,7 @@ export class MakeTransferAccountHttpService {
       .getAggregations({ productKindIds: [accountKind] })
       .pipe(
         map((item) => {
-          const balances =
-            item[0] &&
-            item[0].aggregatedBalances &&
-            item[0].aggregatedBalances[0];
+          const balances = item[0]?.aggregatedBalances?[0];
           return parseInt(balances?.amount || '0', 10);
         }),
         catchError(() => of(300))
