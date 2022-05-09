ae4417cda76266625c7e6e67922e2086eab19ba8
Kirill Sudarushkin <kirills@backbase.com>
2022-05-09 16:15:39 +0200

    Merge with main, fix the build and add proxy for API

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM repo.backbase.com/backbase-docker-releases/web-base:latest
 
 COPY ./dist/apps/golden-sample-app /statics
+COPY ./api-proxy.nginx.conf /nginx-config/server/10-api-proxy.conf
--- libs/transactions/src/index.ts
+++ libs/transactions/src/index.ts
@@ -1,6 +1,8 @@
 export * from './lib/services/transactions-journey-config.service';
 export * from './lib/transactions-journey.module';
+export * from './lib/transactions-journey.component';
 export * from './lib/views/transactions-view/transactions-view.component';
+export * from './lib/directives/transaction-additional-details.directive';
 
 export {
   TransactionsCommunicationService,
