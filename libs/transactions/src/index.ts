228403a4d8a73871cf49a86486b3550f6edd16ab
amirfarzan <amirfarzan@backbase.com>
2022-05-18 16:36:39 +0200

    Fix dependency injection error

--- libs/transactions/src/index.ts
+++ libs/transactions/src/index.ts
@@ -1,3 +1,6 @@
+export * from './lib/transactions-journey.component';
+export * from './lib/directives/transaction-additional-details.directive';
+
 export * from './lib/services/transactions-journey-config.service';
 export * from './lib/transactions-journey.module';
 export * from './lib/views/transactions-view/transactions-view.component';
