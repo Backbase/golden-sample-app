f5d08dec24c73308f5e220b69b2113d2f8bb8ff7
Kirill Sudarushkin <kirills@backbase.com>
2022-07-28 17:38:36 +0200

    resolve errors for ach-positive-pay

--- libs/ach-positive-pay/src/lib/ach-positive-pay-journey.module.ts
+++ libs/ach-positive-pay/src/lib/ach-positive-pay-journey.module.ts
@@ -2,7 +2,7 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { AchPositivePayJourneyComponent } from './views/ach-positive-pay-journey/ach-positive-pay-journey.component';
 import { AchPositivePayRulesComponent } from './views/ach-positive-pay-rules/ach-positive-pay-rules.component';
 import { AchPositivePayNewRuleComponent } from './views/ach-positive-pay-new-rule/ach-positive-pay-new-rule.component';
-import { HeadingWidgetModule } from '@backbase/universal-ang';
+import { HeadingModule } from '@backbase/ui-ang/heading';
 import { AccountSelectorModule } from '@backbase/ui-ang/account-selector';
 import { AlertModule } from '@backbase/ui-ang/alert';
 import { ButtonModule } from '@backbase/ui-ang/button';
@@ -32,7 +32,7 @@ import { AchPositivePayHttpService } from './services/ach-positive-pay.http.serv
     CommonModule,
     RouterModule,
     ReactiveFormsModule,
-    HeadingWidgetModule,
+    HeadingModule,
     EmptyStateModule,
     ModalModule,
     HeaderModule,
