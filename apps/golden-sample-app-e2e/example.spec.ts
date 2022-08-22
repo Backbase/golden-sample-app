38e531ed5ea84424f6983c7c170805c627c4df14
manisha <manisha@backbase.com>
2022-08-22 14:22:58 +0200

    Fixed e2e

--- apps/golden-sample-app-e2e/example.spec.ts
+++ apps/golden-sample-app-e2e/example.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('http://localhost:4200');
-  const title = page.locator('[data-role="headings"]');
-  await expect(title).toHaveText('Welcome to golden sample application');
+  const title = page.locator('#kc-page-title');
+  await expect(title).toHaveText('Welcome to your online banking');
 });
