ab26758bbee1052b6c5ad89b7d9cf6e0cb8e81c9
manisha <manisha@backbase.com>
2022-09-01 10:09:58 +0200

    fix test cases

--- apps/golden-sample-app/src/app/user-context/user-context.component.spec.ts
+++ apps/golden-sample-app/src/app/user-context/user-context.component.spec.ts
@@ -1,14 +1,20 @@
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserContextComponent } from './user-context.component';
+import { Router } from '@angular/router';
 
 describe('UserContextComponent', () => {
   let fixture: ComponentFixture<UserContextComponent>;
-
+  const mockRouter = {
+    navigateByUrl: jasmine.createSpy('navigateByUrl')
+  };
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [UserContextComponent],
       schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: Router, useValue: mockRouter },
+      ]
     }).compileComponents();
   });
 
