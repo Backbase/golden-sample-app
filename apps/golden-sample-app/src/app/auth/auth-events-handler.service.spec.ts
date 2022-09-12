b2bd2da587786351b1f00526620cab7df09775bb
Liam Middleton <liam@backbase.com>
2022-09-12 13:48:26 +0100

    Add Auth Events Handler Service to golden sample app which examples the how auth events could be used in the application to manage Authentication of the user - IDWOJ-1261

---
+++ apps/golden-sample-app/src/app/auth/auth-events-handler.service.spec.ts
@@ -0,0 +1,146 @@
+import { OAuthEvent, OAuthService } from 'angular-oauth2-oidc';
+import { Subject, Subscription } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+import { AuthEventsHandlerService } from './auth-events-handler.service';
+
+export type WidePropertyTypes<T> = Partial<Record<keyof T, any>>;
+export const mock = <T>(overrides?: WidePropertyTypes<T>) =>
+  ({ ...overrides } as jest.Mocked<T>);
+describe('AuthEventsHandlerService', () => {
+  const getInstance = () => {
+    const events$$ = new Subject<OAuthEvent>();
+    const oAuthService = mock<OAuthService>({
+      events: events$$.asObservable(),
+      refreshToken: jest.fn(),
+      revokeTokenAndLogout: jest.fn(),
+      initLoginFlow: jest.fn(),
+    });
+    const service = new AuthEventsHandlerService(oAuthService);
+    const scheduler = new TestScheduler((a, e) => expect(a).toEqual(e));
+
+    return { service, oAuthService, events$$, scheduler };
+  };
+
+  describe('Document load', () => {
+    it('should not handle events if document is not loaded', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'token_expires' });
+        flush();
+      });
+
+      expect(oAuthService.refreshToken).not.toHaveBeenCalled();
+    });
+    it('should handle events when document is loaded', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'token_expires' });
+        flush();
+      });
+
+      expect(oAuthService.refreshToken).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Refreshing access tokens', () => {
+    it('should refresh an access token that expires', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'token_expires' });
+        flush();
+      });
+
+      expect(oAuthService.refreshToken).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Revoking tokens and logging out', () => {
+    it('should revoke tokens and log out the user when refresh token errors', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'token_refresh_error' });
+        flush();
+      });
+
+      expect(oAuthService.revokeTokenAndLogout).toHaveBeenCalledTimes(1);
+    });
+    it('should revoke tokens and log out the user when token errors', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'token_error' });
+        flush();
+      });
+
+      expect(oAuthService.revokeTokenAndLogout).toHaveBeenCalledTimes(1);
+    });
+    it('should revoke tokens and log out the user when code exchange errors', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'code_error' });
+        flush();
+      });
+
+      expect(oAuthService.revokeTokenAndLogout).toHaveBeenCalledTimes(1);
+    });
+    it('should revoke tokens and log out the user when the session errors', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'session_error' });
+        flush();
+      });
+
+      expect(oAuthService.revokeTokenAndLogout).toHaveBeenCalledTimes(1);
+    });
+    it('should revoke tokens and log out the user when the session is terminated', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'session_terminated' });
+        flush();
+      });
+
+      expect(oAuthService.revokeTokenAndLogout).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('Redirecting to login page', () => {
+    it('should redirect the user to the login page when they use an invalid nonce state', () => {
+      const { events$$, oAuthService, scheduler } = getInstance();
+
+      scheduler.run(({ flush }) => {
+        events$$.next({ type: 'discovery_document_loaded' });
+        events$$.next({ type: 'invalid_nonce_in_state' });
+        flush();
+      });
+
+      expect(oAuthService.initLoginFlow).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('#ngOnDestroy', () => {
+    it('should close events subscription', () => {
+      const { service } = getInstance();
+      const subscription: Subscription = (<any>service).eventsSubscription;
+
+      expect(subscription.closed).toBe(false);
+
+      service.ngOnDestroy();
+
+      expect(subscription.closed).toBe(true);
+    });
+  });
+});
---
+++ apps/golden-sample-app/src/app/auth/auth-events-handler.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { OAuthEvent, OAuthService } from 'angular-oauth2-oidc';
+import { Subscription } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthEventsHandlerService implements OnDestroy {
+  private eventsSubscription: Subscription;
+  private documentLoaded = false;
+  constructor(private readonly oAuthService: OAuthService) {
+    this.eventsSubscription = this.getEventsSubscription();
+  }
+
+  /**
+   * Subscribes to the observable provided by the `angular-oauth2-oidc` service for auth events.
+   * Documentation for these events is provided via the library
+   * https://manfredsteyer.github.io/angular-oauth2-oidc/docs/additional-documentation/events.html
+   *
+   * The handling of events follow our best understanding of how we think applications should
+   * behave in each scenario.
+   *
+   * @returns a subscription to the auth service events
+   */
+  private getEventsSubscription() {
+    return this.oAuthService.events.subscribe({
+      next: (event: OAuthEvent) => {
+        // Don't handle any events if the document isn't loaded.
+        if (!this.documentLoaded && event.type !== 'discovery_document_loaded')
+          return;
+
+        switch (event.type) {
+          case 'discovery_document_loaded':
+            this.documentLoaded = true;
+            break;
+          // Expired access tokens are automatically refreshed.
+          case 'token_expires':
+            this.oAuthService.refreshToken();
+            break;
+          // Any authentication failure is treated as a terminal auth issue and the user is logged out.
+          case 'token_refresh_error':
+          case 'token_error':
+          case 'code_error':
+          case 'session_error':
+          case 'session_terminated':
+            this.oAuthService.revokeTokenAndLogout();
+            break;
+          // Invalid login process is treated as a threat and the user is returned to the login page.
+          // As the user is already logged in on the Auth server, they should just be navigated back to the app.
+          case 'invalid_nonce_in_state':
+            this.oAuthService.initLoginFlow();
+            break;
+        }
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.eventsSubscription.unsubscribe();
+  }
+}
