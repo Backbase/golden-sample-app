a1238786b28363595be904874404eb841eaa22cc
Kirill Sudarushkin <kirills@backbase.com>
2022-09-27 14:39:03 +0200

    improve usablility for user accounts

--- apps/golden-sample-app/src/app/user-accounts/user-accounts.module.ts
+++ apps/golden-sample-app/src/app/user-accounts/user-accounts.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { provideRoutes } from '@angular/router';
+import { LoadingIndicatorModule } from '@backbase/ui-ang/loading-indicator';
+import { ProductItemBasicAccountModule } from '@backbase/ui-ang/product-item-basic-account';
 import { UserAccountsViewComponent } from './user-accounts-view/user-accounts-view.component';
 import { UserAccountsService } from './user-accounts.service';
 
 @NgModule({
   declarations: [UserAccountsViewComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    LoadingIndicatorModule,
+    ProductItemBasicAccountModule
+  ],
   providers: [
     provideRoutes([
       {
@@ -17,4 +23,4 @@ import { UserAccountsService } from './user-accounts.service';
     UserAccountsService,
   ],
 })
-export class UserAccountsModule {}
+export class UserAccountsModule { }
