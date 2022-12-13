13fdb6b82e82b385afa79f407e5787a9773d2e32
manisha <manisha@backbase.com>
2022-12-13 16:54:31 +0100

    feature: updated to angular 14

--- apps/golden-sample-app/jest.config.ts
+++ apps/golden-sample-app/jest.config.ts
@@ -1,18 +1,19 @@
-module.exports = {
-  displayName: 'transfer-journey',
+/* eslint-disable */
+export default {
+  displayName: 'golden-sample-app',
   preset: '../../jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/test.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/libs/transfer-journey',
+  coverageDirectory: '../../coverage/apps/golden-sample-app',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
