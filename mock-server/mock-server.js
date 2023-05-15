aa05d7aa52aae9d688ee8b9a140e5df993eea37d
Ankit Pant <ankit12.pant@gmail.com>
2023-05-15 17:31:04 +0200

    chore: add doc comment for ng-apimock (#171)
    * chore: add doc comment for ng-apimock

--- mock-server/mock-server.js
+++ mock-server/mock-server.js
@@ -4,22 +4,26 @@ const express = require('express');
 const app = express();
 
 const appName = process.argv[3];
-if (!appName) throw 'Provide a name of an app you want to start mocks for!';
+if (!appName) throw 'Provide a name of an app you want to start mocks for!'; // This is the app name for which you want to run mocks
 
-console.log(appName);
+console.log(`Starting the mocks server for ${appName}`);
 
-app.set('port', 9999);
+app.set('port', 9999); // setting the mock server port to 9999
 
+/**
+ * For detailed explanation please visit
+ * https://ngapimock.org/docs/installation#processor
+ */
 apiMock.processor.process({
-  src: 'mock-server/mocks',
+  src: 'mock-server/mocks', // Place where to find the mocks
   patterns: {
-    mocks: `${appName}/**/*.json`,
-    presets: '**/*.preset.json',
+    mocks: `${appName}/**/*.json`, // Pattern of the mock files
+    presets: '**/*preset.json',
   },
   watch: true,
 });
 
-app.use(apiMock.middleware);
+app.use(apiMock.middleware); // Register @ng-apimock/core as middleware
 app.use('/dev-interface', express.static(devInterface));
 
 app.listen(app.get('port'), () => {
