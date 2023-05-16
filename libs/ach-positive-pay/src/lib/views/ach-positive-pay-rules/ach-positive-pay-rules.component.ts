f20526773551114dd00d50d18ac1d3a21006f9b7
manisha <manisha@backbase.com>
2023-05-16 16:28:38 +0200

    fix: removed libs not following DDD structure

--- libs/ach-positive-pay/src/lib/views/ach-positive-pay-rules/ach-positive-pay-rules.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bb-ach-positive-pay-rules',
-  templateUrl: './ach-positive-pay-rules.component.html',
-})
-export class AchPositivePayRulesComponent {}
