2f771ce7e66bee4b53c469b23f85bc9284320fe9
manisha <manisha@backbase.com>
2023-09-18 16:14:55 +0200

    chore: fixed formatting

--- libs/transactions-journey/shell/src/lib/transactions-journey-shell.module.ts
+++ libs/transactions-journey/shell/src/lib/transactions-journey-shell.module.ts
@@ -18,7 +18,12 @@ import {
   TransactionsRouteTitleResolverService,
 } from '@backbase-gsa/internal-transactions-data-access';
 import { TransactionDetailsComponent } from '@backbase/internal-transactions-feature-transaction-details-view';
-import { TRANSACTION_EXTENSIONS_CONFIG, TransactionsJourneyExtensionsConfig, TransactionsViewComponent, TransactionsViewModule } from '@backbase/internal-transactions-feature-transaction-view';
+import {
+  TRANSACTION_EXTENSIONS_CONFIG,
+  TransactionsJourneyExtensionsConfig,
+  TransactionsViewComponent,
+  TransactionsViewModule,
+} from '@backbase/internal-transactions-feature-transaction-view';
 const defaultRoutes: Routes = [
   {
     path: '',
