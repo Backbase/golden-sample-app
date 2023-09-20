2932903e781a5ac958c940fa1cd9533341d64bf7
ankypant <ankit12.pant@gmail.com>
2023-09-20 12:27:57 +0200

    chore: update otel config

--- apps/golden-sample-app/src/app/services/analytics.service.ts
+++ apps/golden-sample-app/src/app/services/analytics.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { TrackerHandler } from '@backbase/foundation-ang/observability';
-import packageInfo from 'package-json';
-import { environment } from '../../environments/environment';
 
 /*
 This service will receive all the analytics events from your application and from here you can
@@ -10,16 +8,6 @@ send all your tracker events to the analytics system (eg: google analytics/segme
 @Injectable()
 export class AnalyticsService extends TrackerHandler {
   register(): void {
-    this.openTelemetryConfig = {
-      appName: packageInfo.name,
-      appVersion: packageInfo.version,
-      apiKey: environment.bbApiKey,
-      env: 'local',
-      isProduction: environment.production,
-      isEnabled: environment.isTelemetryTracerEnabled,
-      url: environment.telemetryCollectorURL,
-    };
-
     this.tracker.subscribeAll((event) => {
       console.log('EVENT TRACKER', event);
     });
