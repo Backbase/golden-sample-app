2e21355a19732690be17cefae4febf824968e4d5
Roman Iovlev <romani@backbase.com>
2024-02-22 10:14:08 +0100

    small fixes

--- apps/golden-sample-app-e2e/page-objects/ui-components/_base-component.ts
+++ apps/golden-sample-app-e2e/page-objects/ui-components/_base-component.ts
@@ -3,19 +3,19 @@ import { LocatorOptions } from '../../utils/locator-options';
 import { isLocator } from 'apps/golden-sample-app-e2e/utils/playwright-utils';
 
 export abstract class BaseComponent {
-  get page(): Page { 
-    if (isLocator(this.accessor)) 
-      throw new Error('Root locator is not defined');
-    return this.accessor as Page; 
+  get page(): Page {
+    if (isLocator(this.accessor))
+      throw new Error('Page is not defined');
+    return this.accessor as Page;
   }
   get root(): Locator {
-    if (!isLocator(this.accessor)) 
+    if (!isLocator(this.accessor))
       throw new Error('Root locator is not defined');
-    return this.accessor as Locator; 
+    return this.accessor as Locator;
   }
 
   constructor(private accessor: Page | Locator) { }
-  
+
   $(selector: string, options?: LocatorOptions): Locator {
     return this.root.locator(selector, options);
   }
--- apps/golden-sample-app-e2e/utils/playwright-utils.ts
+++ apps/golden-sample-app-e2e/utils/playwright-utils.ts
@@ -2,4 +2,3 @@ import { Locator } from '@playwright/test';
 
 export const isLocator = (param: any): param is Locator =>
   typeof param === 'object' && param.toString().split('@')[0] === 'Locator';
-  
