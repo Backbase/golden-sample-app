b3f458629fc03b2b0d976645c884a87d8b82926f
Edward Rothwell <edwardro@backbase.com>
2024-06-28 15:19:19 +0100

    Added visual test example with masked elements

--- apps/golden-sample-app-e2e/specs/transactions.spec.ts
+++ apps/golden-sample-app-e2e/specs/transactions.spec.ts
@@ -1,10 +1,17 @@
 import { test } from '../page-objects/test-runner';
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('transactions');
+});
 test.describe.configure({ mode: 'parallel' });
 
 test.describe('@feature @responsive @visual Transaction page tests', () => {
-  test('Validate responsiveness of the transactions page', async ({ page, visual }) => {
-    await page.goto('transactions');
-    await visual.testFullPage('transactions-page');
+  test.only('Validate responsiveness of the transactions page', async ({ page, visual }) => {
+    await test.step(`Transaction page full`, async () => {
+      await visual.testFullPage('transactions-page-full');
+    });
+    await test.step(`Transaction page with masked data`, async () => {
+      await visual.testWithMask('transactions-page-with-masked-data', { mask: ['.transaction-account-number']});
+    });
   });
 });
