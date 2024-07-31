c26b46e64e87537a655e8c55d9e2d5a44122c889
Shruti Srivastava <shruti91sri@users.noreply.github.com>
2024-08-01 00:00:37 +0200

    Feature/PXS-6168 add premium theme (#238)
    * test
    
    * 1st draft of multi theming
    
    * Final draft
    
    * add pack-lock.json
    
    * update as per comments
    
    * fix formating
    
    * fix styles in project.json
    
    * remove commented lines
    
    * add global override scss to theme-premium
    
    ---------
    
    Co-authored-by: Ankit Pant <ankit12.pant@gmail.com>

---
+++ apps/golden-sample-app/src/app/theme-switcher/theme-switcher.component.module.ts
@@ -0,0 +1,10 @@
+import { DropdownMenuModule } from '@backbase/ui-ang/dropdown-menu';
+import { ThemeSwitcherComponent } from './theme-switcher.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [ThemeSwitcherComponent],
+  imports: [DropdownMenuModule],
+  exports: [ThemeSwitcherComponent],
+})
+export class ThemeSwitcherModule {}
---
+++ apps/golden-sample-app/src/app/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { ThemeManagerService } from './theme-service';
+
+@Component({
+  selector: 'app-theme-switcher',
+  templateUrl: './theme-switcher.component.html',
+})
+export class ThemeSwitcherComponent {
+  constructor(private themeManagerService: ThemeManagerService) {}
+
+  @Input() theme = ThemeManagerService.THEMES[0];
+
+  protected themes = ThemeManagerService.THEMES;
+
+  protected onSelect(theme: string | object) {
+    if (typeof theme === 'string') {
+      this.themeManagerService.setTheme(theme);
+      this.theme = theme;
+    }
+  }
+}
