3f04c3731e9a3cd0e35d419b303e03d7977c9840
tomas_backbase <tomas@backbase.com>
2024-09-20 12:08:05 +0200

    Add tracking to app error handler

--- apps/golden-sample-app/src/app/app.error-handler.ts
+++ apps/golden-sample-app/src/app/app.error-handler.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { ErrorHandler, Injectable, NgZone } from '@angular/core';
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
+import { Tracker, TrackerEvent, TrackerEventBase, TrackerEventPayload } from '@backbase/foundation-ang/observability';
 
 function extractRejectionError(error: unknown): Error | unknown {
   if (error && typeof error === 'object' && 'rejection' in error) {
@@ -10,14 +11,33 @@ function extractRejectionError(error: unknown): Error | unknown {
   return error;
 }
 
+
+type Optional<Type, Key extends keyof Type> = Omit<Type, Key> & Partial<Pick<Type, Key>>;
+
+export interface UnhandledErrorTrackerEventPayload extends TrackerEventPayload {
+  message: string;
+  stack: string;
+}
+
+export class UnhandledErrorTrackerEvent<
+  T extends Optional<TrackerEventBase, 'name'> = { payload: UnhandledErrorTrackerEventPayload },
+> extends TrackerEvent<T['name'] & string, NonNullable<T['payload']>> {
+  readonly name = 'unhandled-error';
+  constructor(payload: NonNullable<T['payload']>, journey?: string) {
+    super(payload, journey);
+  }
+}
+
 @Injectable()
 export class AppErrorHandler implements ErrorHandler {
   constructor(
     private readonly router: Router,
-    private readonly ngZone: NgZone
+    private readonly ngZone: NgZone,
+    private injector: Injector
   ) {}
 
   handleError(error: Error | unknown): void {
+    const tracker = this.injector.get(Tracker);
     const extractedError = extractRejectionError(error);
 
     if (extractedError instanceof HttpErrorResponse) {
@@ -33,6 +53,20 @@ export class AppErrorHandler implements ErrorHandler {
       return;
     }
 
+    if (error instanceof Error) {
+      // Publish the error using your Tracker service
+      tracker?.publish(new UnhandledErrorTrackerEvent({ 
+        message: error?.message,
+        stack: error?.stack || 'Unknown error stack',
+      }));
+    } else {
+      // Publish the error using your Tracker service
+      tracker?.publish(new UnhandledErrorTrackerEvent({ 
+        message: 'Unknown error message',
+        stack: 'Unknown error stack',
+      }));
+    }
+    
     console.error(error);
   }
 }
