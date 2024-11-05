f599b87c864f5f8cd830bf05cc88151d9f7d1e90
ankypant <ankit12.pant@gmail.com>
2024-11-05 17:25:20 +0100

    chore: update proxy config to use js extension

--- apps/golden-sample-app/proxy.config.js
+++ apps/golden-sample-app/proxy.config.js
@@ -1,13 +1,11 @@
-const proxyConfig = [
+module.exports = [
   {
     context: '/api',
     target: 'https://app.stg.sdbxaz.azure.backbaseservices.com',
-    secure: false,
+    secure: true,
     changeOrigin: true,
     headers: {
       'X-SDBXAZ-API-KEY': process.env['X-SDBXAZ-API-KEY'],
     },
   },
 ];
-
-module.exports = proxyConfig;
