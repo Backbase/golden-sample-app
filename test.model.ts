de23f32da67887e1812a51d2285041b97153974c
vatsala-glory <31056784+vatsala-glory@users.noreply.github.com>
2024-12-13 08:06:49 +0000

    tests using config to determine data (#357)
    Co-authored-by: Vatsala <vatsala@backbase.com>

--- test.model.ts
+++ test.model.ts
@@ -1,3 +1,11 @@
 export interface TestOptions{
- configPath: string;   
+ configPath: string;
+ testEnvironment: TestEnvironment;
+}
+
+export enum TestEnvironment {
+    MOCKS= 'mocks',
+    SANDBOX = 'sandbox',
+    EPHEMERAL = 'ephemeral',
+    MODELBANK_STAGING = 'mb-stg'
 }
