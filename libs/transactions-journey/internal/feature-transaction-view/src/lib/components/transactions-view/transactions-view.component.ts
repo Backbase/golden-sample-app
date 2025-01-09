f6226451538a75cd19cedc26523e173f495fdc6d
Telman Ahababov <telman.agababov@gmail.com>
2025-01-09 08:48:19 +0100

    Update journeys folders structure - Bundle journeys by domain (#356)
    Bundle journeys by domain
    
    Co-authored-by: vatsala-glory <31056784+vatsala-glory@users.noreply.github.com>
    Co-authored-by: Tomás Corral Casas <amischol@gmail.com>

---
+++ libs/transactions-journey/internal/feature-transaction-view/src/lib/components/transactions-view/transactions-view.component.ts
@@ -0,0 +1,90 @@
+import { Component, Inject, Optional } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { combineLatest, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import {
+  ScreenViewTrackerEventPayload,
+  Tracker,
+} from '@backbase/foundation-ang/observability';
+
+import {
+  TransactionsCommunicationService,
+  TRANSACTIONS_JOURNEY_COMMUNICATION_SERIVCE,
+  ArrangementsService,
+  TransactionsHttpService,
+} from '@backbase-gsa/transactions-journey/internal/data-access';
+
+import { TransactionListTrackerEvent } from '@backbase-gsa/transactions-journey/internal/shared-data';
+@Component({
+  templateUrl: './transactions-view.component.html',
+  styleUrls: ['./transactions-view.component.scss'],
+  selector: 'bb-transactions-view',
+})
+export class TransactionsViewComponent {
+  public title = this.route.snapshot.data['title'];
+
+  public filter = '';
+
+  private readonly accountId$ = this.route.queryParamMap.pipe(
+    map((params) => params.get('account'))
+  );
+
+  public accountName$ = combineLatest({
+    accountId: this.accountId$,
+    accounts: this.arrangementsService.arrangements$,
+  }).pipe(
+    map(({ accountId, accounts }) => accounts.find((x) => x.id === accountId)),
+    map((account) => account?.bankAlias ?? '')
+  );
+
+  public transactions$ = combineLatest({
+    transactions: this.transactionsService.transactions$,
+    transfer:
+      this.externalCommunicationService?.latestTransaction$ ?? of(undefined),
+    accountId: this.accountId$,
+  }).pipe(
+    map(({ transactions = [], transfer, accountId }) => {
+      transactions = [...transactions];
+
+      if (transfer) {
+        transactions.unshift(transfer);
+      }
+
+      if (accountId) {
+        transactions = transactions.filter(
+          (item) => item.arrangementId === accountId
+        );
+      }
+
+      return transactions;
+    })
+  );
+
+  public searchQuery$ = this.route.queryParamMap.pipe(
+    map((params) => params.get('search') ?? '')
+  );
+
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+    private readonly transactionsService: TransactionsHttpService,
+    private readonly arrangementsService: ArrangementsService,
+    @Optional()
+    @Inject(TRANSACTIONS_JOURNEY_COMMUNICATION_SERIVCE)
+    private externalCommunicationService: TransactionsCommunicationService,
+    @Optional() private tracker?: Tracker
+  ) {}
+
+  search(ev: string) {
+    this.filter = ev || '';
+    this.router.navigate([], {
+      queryParams: { search: this.filter || undefined },
+      queryParamsHandling: 'merge',
+    });
+  }
+
+  trackNavigation($event: ScreenViewTrackerEventPayload) {
+    this.tracker?.publish(new TransactionListTrackerEvent($event));
+  }
+}
