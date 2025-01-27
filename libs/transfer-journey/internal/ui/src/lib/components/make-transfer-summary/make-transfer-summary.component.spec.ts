f2c9521bfb8ae0892f35c8bc684dc450e768bcf2
tcorral <amischol@gmail.com>
2025-01-27 16:45:27 +0100

    Fix tests

--- libs/transfer-journey/internal/ui/src/lib/components/make-transfer-summary/make-transfer-summary.component.spec.ts
+++ libs/transfer-journey/internal/ui/src/lib/components/make-transfer-summary/make-transfer-summary.component.spec.ts
@@ -1,9 +1,11 @@
+import "@angular/localize/init";
 import { MakeTransferSummaryComponent } from './make-transfer-summary.component';
+import { Translations } from './translations.provider';
 
 describe('MakeTransferSummaryComponent', () => {
   let component: MakeTransferSummaryComponent;
   beforeEach(() => {
-    component = new MakeTransferSummaryComponent();
+    component = new MakeTransferSummaryComponent({} as Translations);
   });
 
   it('should create', () => {
