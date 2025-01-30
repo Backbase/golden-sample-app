7a3b01a744afa7d6049d7badb607f7f2fc78cc05
tcorral <amischol@gmail.com>
2025-01-30 11:17:56 +0100

    Add colon right after the Template start for a proper usage of

--- apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.ts
+++ apps/golden-sample-app/src/app/user-accounts/user-accounts-view/user-accounts-view.component.ts
@@ -23,34 +23,34 @@ export class UserAccountsViewComponent {
 
   public readonly translations: Translations = {
     'app.userAccountsView.header':
-      $localize`User accounts header - 'User accounts'|This string is used as the
+      $localize`:User accounts header - 'User accounts'|This string is used as the
           header for the user accounts view page. It is presented to the user as
           the title of the page when they view their user accounts. This header
           is located at the top of the user accounts view
           page.@@app.userAccountsView.header:User accounts`,
     'app.userAccountsView.balanceLabel':
-      $localize`User account available balance label - 'Available balance'|This
+      $localize`:User account available balance label - 'Available balance'|This
               string is used as the label for the available balance field in the
               user accounts view. It is presented to the user to indicate the
               available balance of their account. This label is located in the
               body section of the user accounts view
               page.@@app.userAccountsView.balanceLabel:Available balance`,
     'user-accounts.view-transactions':
-      $localize`Label for View Transactions link - 'View Transactions'|This string
+      $localize`:Label for View Transactions link - 'View Transactions'|This string
               is used as the label for a link that navigates to the transactions
               page. It is presented to the user as a link to view transactions
               related to a specific account. This label is located in the body
               section of the user accounts view
               page.@@user-accounts.view-transactions:View Transactions`,
     'user-accounts.add-favorites':
-      $localize`Label for Add to Favorites link - 'Add to Favorites'|This string
+      $localize`:Label for Add to Favorites link - 'Add to Favorites'|This string
                 is used as the label for a link that adds an account to the
                 user's favorites. It is presented to the user as a link to mark
                 an account as a favorite. This label is located in the body
                 section of the user accounts view
                 page.@@user-accounts.add-favorites:Add to Favorites`,
     'user-accounts.remove-favorites':
-      $localize`Label for Remove from Favorites link - 'Remove from
+      $localize`:Label for Remove from Favorites link - 'Remove from
                 Favorites'|This string is used as the label for a link that
                 removes an account from the user's favorites. It is presented to
                 the user as a link to unmark an account as a favorite. This
