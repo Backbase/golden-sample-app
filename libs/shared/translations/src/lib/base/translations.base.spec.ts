ac761e1169814fa0f3a33e7a42093334bb5fbdf3
tcorral <amischol@gmail.com>
2025-02-04 15:38:00 +0100

    Add new tests for the new library

---
+++ libs/shared/translations/src/lib/base/translations.base.spec.ts
@@ -0,0 +1,45 @@
+import { TranslationsBase } from './translations.base';
+import { TranslationRecord } from '../types/translations.types';
+
+interface MockTranslationRecord extends TranslationRecord {
+  key1: string;
+  key2: string;
+}
+
+class MockTranslations extends TranslationsBase<MockTranslationRecord> {}
+
+describe('TranslationsBase', () => {
+  const defaultTranslations: MockTranslationRecord = {
+    key1: 'default value 1',
+    key2: 'default value 2',
+  };
+
+  it('should use default translations when custom translations are null', () => {
+    const translations = new MockTranslations(defaultTranslations, null);
+    expect(translations.translations).toEqual(defaultTranslations);
+  });
+
+  it('should merge custom translations with default translations', () => {
+    const customTranslations: Partial<MockTranslationRecord> = {
+      key1: 'custom value 1',
+    };
+    const expectedTranslations: MockTranslationRecord = {
+      key1: 'custom value 1',
+      key2: 'default value 2',
+    };
+    const translations = new MockTranslations(defaultTranslations, customTranslations);
+    expect(translations.translations).toEqual(expectedTranslations);
+  });
+
+  it('should use default value if custom translation key is undefined', () => {
+    const customTranslations: Partial<MockTranslationRecord> = {
+      key1: undefined,
+    };
+    const expectedTranslations: MockTranslationRecord = {
+      key1: 'default value 1',
+      key2: 'default value 2',
+    };
+    const translations = new MockTranslations(defaultTranslations, customTranslations);
+    expect(translations.translations).toEqual(expectedTranslations);
+  });
+});
--- libs/shared/translations/src/lib/index.ts
+++ libs/shared/translations/src/lib/index.ts
@@ -1,3 +1,2 @@
 export * from './base/translations.base';
-export * from './decorators/translations.decorator';
 export * from './types/translations.types';
