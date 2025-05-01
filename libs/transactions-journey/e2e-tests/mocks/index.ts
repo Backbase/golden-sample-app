f5f5f53cb93fc4b8e321dd314fc14a3cfd83d304
Roman <romani@backbase.com>
2025-05-01 13:35:54 +0200

    update mocks

--- libs/transactions-journey/e2e-tests/mocks/index.ts
+++ libs/transactions-journey/e2e-tests/mocks/index.ts
@@ -1,6 +1,8 @@
-import { Page } from '@playwright/test';
-import { readFileSync } from 'node:fs';
+import { test, Page } from '@playwright/test';
+import { readFileSync, writeFileSync } from 'node:fs';
 import { join } from 'node:path';
+import { TransactionDetailsDataType } from '../model';
+import { createTransactionDetailsForMock } from '../model/transaction';
 
 const accessControlMocks = {
   '**/accessgroups/users/permissions/summary': loadMockFile(
@@ -25,18 +27,38 @@ export const defaultTransactionsMock = {
   ...transactionsMocks,
 };
 
-export const defaultTransactionMock = {
-  ...accessControlMocks,
-  ...arrangementsMocks,
-  ...transactionsMocks,
-};
-
 export function loadMockFile<T>(path: string): T {
   return JSON.parse(readFileSync(join(__dirname, path), 'utf-8'));
 }
 
+export function writeMockFile<T>(path: string, data: T): void {
+  writeFileSync(join(__dirname, path), JSON.stringify(data, null, 2), 'utf-8');
+}
+
 export function setupPageMocks(page: Page, mocks: Record<string, unknown>) {
-  for (const [endpoint, data] of Object.entries(mocks ?? {})) {
-    page.route(endpoint, (route) => route.fulfill({ json: data }));
+  test.step('Setup mock for transactions', async () => {
+    for (const [endpoint, data] of Object.entries(mocks ?? {})) {
+      page.route(endpoint, (route) => route.fulfill({ json: data }));
+    }
+  });
+}
+
+export function addTransactionsToMock(
+  transactions: Partial<TransactionDetailsDataType>[]
+) {
+  for (const transaction of transactions) {
+    const transactionsList = loadMockFile<{ id: string }[]>(
+      './transactions-list.json'
+    );
+    const newTransaction = createTransactionDetailsForMock(transaction);
+    const existingIndex = transactionsList.findIndex(
+      (t) => t.id === newTransaction.id
+    );
+    const indexToUpdate =
+      existingIndex >= 0 ? existingIndex : transactionsList.length;
+    transactionsList[indexToUpdate] = newTransaction;
+    writeMockFile('./transactions-list.json', transactionsList);
   }
+
+  return defaultTransactionsMock;
 }
