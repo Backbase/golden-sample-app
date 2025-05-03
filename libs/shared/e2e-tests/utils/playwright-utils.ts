d773b4a0d863166d779ea7554783a251e732a4eb
Roman <romani@backbase.com>
2025-05-03 02:07:06 +0200

    remove generic classes

--- libs/shared/e2e-tests/utils/playwright-utils.ts
+++ libs/shared/e2e-tests/utils/playwright-utils.ts
@@ -15,34 +15,3 @@ export function getSelector(locator?: string | Locator): string {
 
   return locatorAsString.substring(index);
 }
-
-export async function collectDataByTestId<
-  T extends Record<string, any> = Record<string, any>
->(
-  page: Page,
-  testIds: string[],
-  getContent: (element: Locator) => Promise<any> = (element) =>
-    element.textContent()
-): Promise<T> {
-  const elements: Locator[] = testIds.map((testId) => page.getByTestId(testId));
-  const contents: (string | null)[] = (
-    await Promise.all(elements.map(getContent))
-  ).filter((value) => value?.trim() ?? '');
-  return Object.fromEntries(
-    testIds.map((testId, i) => [testId, contents[i] || undefined])
-  ) as T;
-}
-
-export function formatData<T extends Record<string, any>>(
-  data: T,
-  formatters: Map<string, (value: any) => string>
-): T {
-  return Object.fromEntries(
-    Object.entries(data).map(([key, value]) => [
-      key,
-      formatters.has(key) && value !== undefined
-        ? formatters.get(key)!(value)
-        : value,
-    ])
-  ) as T;
-}
