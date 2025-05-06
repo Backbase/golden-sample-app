b37ab0439ac9146f6b9396a82e19df2fb72b36c6
tcorral <amischol@gmail.com>
2025-05-06 19:36:34 +0200

    Temporary commit

--- libs/journey-bundles/custom-payment/src/lib/initiate-payment-journey-bundle.module.ts
+++ libs/journey-bundles/custom-payment/src/lib/initiate-payment-journey-bundle.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 import {
   INITIATE_PAYMENT_CONFIG,
   InitiatePaymentJourneyModule,
+  ӨReviewPaymentService
 } from '@backbase/initiate-payment-journey-ang';
 import { AccountSelectorModule } from '@backbase/ui-ang/account-selector';
+import { Router } from '@angular/router';
 import { InitiatorComponent } from './components/initiator/initiator.component';
-import { customPaymentConfig } from './custom-payment.config';
+import { customPaymentConfig, setRouter } from './custom-payment.config';
+
+// Create a provider for the review service
+export function reviewServiceFactory(router: Router) {
+  return {
+    navigateFromSuccess: () => {
+      router.navigate(['/transactions']);
+    },
+    navigateFromCancel: () => {
+      router.navigate(['/transactions']);
+    },
+    navigateFromError: () => {
+      router.navigate(['/error']);
+    },
+    isInModal: () => false
+  };
+}
 
 @NgModule({
   /**
@@ -23,6 +41,16 @@ import { customPaymentConfig } from './custom-payment.config';
       provide: INITIATE_PAYMENT_CONFIG,
       useValue: customPaymentConfig,
     },
+    {
+      provide: ӨReviewPaymentService,
+      useFactory: reviewServiceFactory,
+      deps: [Router]
+    }
   ],
 })
-export class CustomPaymentJourneyBundleModule {}
+export class CustomPaymentJourneyBundleModule {
+  constructor(router: Router) {
+    // Inject router into the config for hooks
+    setRouter(router);
+  }
+}
