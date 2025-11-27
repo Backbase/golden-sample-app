b1d129af6182f4e9804282c8c3bca286be4a66f7
tcorral <amischol@gmail.com>
2025-11-27 12:19:30 +0100

    chore: applying code review suggestions

--- apps/golden-sample-app/src/app/error-page/error-page.component.ts
+++ apps/golden-sample-app/src/app/error-page/error-page.component.ts
@@ -1,13 +1,12 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { ErrorCommonStateModule } from '@backbase/ui-ang/common-error-state';
 
 @Component({
   selector: 'app-error-page',
   templateUrl: './error-page.component.html',
-  imports: [CommonModule, ErrorCommonStateModule],
+  imports: [ErrorCommonStateModule],
 })
 export default class ErrorPageComponent {
   private readonly router: Router = inject(Router);
