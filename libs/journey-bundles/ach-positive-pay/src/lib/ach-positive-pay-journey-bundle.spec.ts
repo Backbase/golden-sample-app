28b3a3a2c74bf4bfd5e80e0fbccf99c1fb9cd397
tomas_backbase <tomas@backbase.com>
2025-11-28 20:16:47 +0100

    chore: adjust documentation to be checked against implementation code

--- libs/journey-bundles/ach-positive-pay/src/lib/ach-positive-pay-journey-bundle.spec.ts
+++
@@ -1,8 +0,0 @@
-import achPositivePayBundle from './ach-positive-pay-journey-bundle';
-
-describe('AchPositivePayJourneyBundle', () => {
-  it('should provide routes for lazy loading', () => {
-    expect(achPositivePayBundle).toBeDefined();
-    expect(achPositivePayBundle.length).toBeGreaterThan(0);
-  });
-});
