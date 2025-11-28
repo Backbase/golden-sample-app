eb3b01a666f9b92c2947f8c30ebc65e6d2cc24cd
tomas_backbase <tomas@backbase.com>
2025-11-28 10:39:45 +0100

    chore: apply suggested changes in pull request

--- libs/shared/feature/user-context/src/index.ts
+++ libs/shared/feature/user-context/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/shared-user-context.providers';
 export * from './lib/shared-user-context.guard';
 export * from './lib/shared-user-context.service';
 export * from './lib/shared-user-context.interceptor';
--- libs/shared/feature/user-context/src/lib/shared-user-context.providers.ts
+++
@@ -1,7 +0,0 @@
-import { SharedUserContextService } from './shared-user-context.service';
-import { SharedUserContextGuard } from './shared-user-context.guard';
-
-export const SHARED_USER_CONTEXT_PROVIDERS = [
-  SharedUserContextService,
-  SharedUserContextGuard,
-];
