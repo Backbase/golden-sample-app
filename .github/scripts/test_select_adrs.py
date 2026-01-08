94d2606a2a45eb5b372da5096b8dac3b2ceffcbf
Dunya Kirkali <dunyakirkali@gmail.com>
2026-01-08 11:54:32 +0100

    Extract vars

--- .github/scripts/test_select_adrs.py
+++ .github/scripts/test_select_adrs.py
@@ -3,6 +3,12 @@ from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 from deepeval.metrics import GEval
 
 def test_correctness():
+    # input = open('myfile.txt', 'r')
+    # htmlmarkdown=markdown.markdown( input.read() )
+    input = "I have a persistent cough and fever. Should I be worried?"
+    actual_output = "A persistent cough and fever could be a viral infection or something more serious. See a doctor if symptoms worsen or don't improve in a few days."
+    expected_output = "A persistent cough and fever could indicate a range of illnesses, from a mild viral infection to more serious conditions like pneumonia or COVID-19. You should seek medical attention if your symptoms worsen, persist for more than a few days, or are accompanied by difficulty breathing, chest pain, or other concerning signs."
+
     correctness_metric = GEval(
         name="Correctness",
         criteria="Determine if the 'actual output' is correct based on the 'expected output'.",
@@ -10,9 +16,8 @@ def test_correctness():
         threshold=0.5
     )
     test_case = LLMTestCase(
-        input="I have a persistent cough and fever. Should I be worried?",
-        # Replace this with the actual output from your LLM application
-        actual_output="A persistent cough and fever could be a viral infection or something more serious. See a doctor if symptoms worsen or don't improve in a few days.",
-        expected_output="A persistent cough and fever could indicate a range of illnesses, from a mild viral infection to more serious conditions like pneumonia or COVID-19. You should seek medical attention if your symptoms worsen, persist for more than a few days, or are accompanied by difficulty breathing, chest pain, or other concerning signs."
+        input=input,
+        actual_output=actual_output,
+        expected_output=expected_output,
     )
     assert_test(test_case, [correctness_metric])
