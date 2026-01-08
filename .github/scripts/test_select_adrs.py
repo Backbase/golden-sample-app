9de622a58d6c72fb2dc3682bfe00ae97110dacb2
Dunya Kirkali <dunyakirkali@gmail.com>
2026-01-08 11:56:54 +0100

    Read prompt

--- .github/scripts/test_select_adrs.py
+++ .github/scripts/test_select_adrs.py
@@ -1,10 +1,12 @@
+import markdown
+
 from deepeval import assert_test
 from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 from deepeval.metrics import GEval
 
 def test_correctness():
-    # input = open('myfile.txt', 'r')
-    # htmlmarkdown=markdown.markdown( input.read() )
+    raw_input = open('docs/prompts/1.1-select-adrs.md', 'r')
+    input = markdown.markdown( raw_input.read() )
     input = "I have a persistent cough and fever. Should I be worried?"
     actual_output = "A persistent cough and fever could be a viral infection or something more serious. See a doctor if symptoms worsen or don't improve in a few days."
     expected_output = "A persistent cough and fever could indicate a range of illnesses, from a mild viral infection to more serious conditions like pneumonia or COVID-19. You should seek medical attention if your symptoms worsen, persist for more than a few days, or are accompanied by difficulty breathing, chest pain, or other concerning signs."
