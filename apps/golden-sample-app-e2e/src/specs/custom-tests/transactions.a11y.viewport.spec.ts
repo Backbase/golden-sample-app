7c33a76b49689bb2193eba63086152259d60d435
Tomasz Kasowicz <tomasz.kasowicz@gmail.com>
2026-06-16 17:54:25 +0200

    fix: reflow in transactions component

---
+++ apps/golden-sample-app-e2e/src/specs/custom-tests/transactions.a11y.viewport.spec.ts
@@ -0,0 +1,24 @@
+import { test } from '../../fixtures/test';
+import { expect } from '../../expect/expect';
+
+test.describe(
+  'Transaction Page A11y Viewport tests',
+  { tag: ['@a11y', '@e2e', '@mocks', '@viewport'] },
+  () => {
+    test.use({ viewport: { width: 320, height: 256 } });
+
+    test.beforeEach(async ({ transactionsPage }) => {
+      await transactionsPage.open();
+      await expect(transactionsPage.pageHeader).toBeVisible();
+      await expect(transactionsPage.transactions.element.first()).toBeVisible();
+    });
+
+    test('Validate Overflows for transactions list', async ({ page }) => {
+      const transactionsView = page.locator('bb-transactions-view');
+
+      await test.step('Validate Overflows for transactions list', async () => {
+        await expect(transactionsView).not.toOverflowViewPort();
+      });
+    });
+  }
+);
