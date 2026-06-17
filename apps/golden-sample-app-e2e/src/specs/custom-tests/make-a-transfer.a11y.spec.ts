ed1f76466a9a04f4eaf8dfab2f863aae73028c00
Tomasz Kasowicz <tomasz.kasowicz@gmail.com>
2026-06-17 08:15:23 +0200

    chore: update eslint, really add tests for make a transfer

---
+++ apps/golden-sample-app-e2e/src/specs/custom-tests/make-a-transfer.a11y.spec.ts
@@ -0,0 +1,52 @@
+import { test } from '../../fixtures/test';
+import { expect } from '../../expect/expect';
+import { TestInfo } from '@playwright/test';
+
+test.describe(
+  'Make a Transfer Page A11y tests',
+  { tag: ['@a11y', '@e2e', '@mocks'] },
+  () => {
+    test.beforeEach(async ({ makeTransferPage }) => {
+      await makeTransferPage.open();
+      await expect(makeTransferPage.pageHeader).toBeVisible();
+    });
+
+    test(
+      'Validate Make a Transfer page accessibility',
+      { tag: ['@axe'] },
+      async ({ makeTransferPage }, testInfo: TestInfo) => {
+        await expect({
+          page: makeTransferPage.page,
+          testInfo: testInfo,
+        }).toBeAccessible();
+      }
+    );
+
+    test(
+      'Validate Make a Transfer tab Order',
+      { tag: ['@tab-order'] },
+      async ({ makeTransferPage, page }) => {
+        const toAccount = page.getByRole('textbox', { name: 'To Account' });
+        await toAccount.focus();
+        const locator = makeTransferPage.locator('bb-transfer-journey');
+        await expect(locator).toHaveFocusOrder([
+          { tagName: 'input', textContent: '' }, // To Account
+          { tagName: 'select', textContent: 'USD USD EUR' }, // Currency Input
+          { tagName: 'input', textContent: '' }, // Integer Input
+          { tagName: 'input', textContent: '' }, // Decimal Input
+          { tagName: 'button', textContent: 'Submit' }, // Submit Button
+        ]);
+      }
+    );
+
+    test(
+      'Validate Make a Transfer page reflow',
+      { tag: ['@reflow'] },
+      async ({ makeTransferPage, page }) => {
+        await page.setViewportSize({ width: 320, height: 256 });
+        const locator = makeTransferPage.locator('bb-transfer-journey');
+        await expect(locator).not.toOverflowViewPort();
+      }
+    );
+  }
+);
